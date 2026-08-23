2774971ab380a52d293ff8f0705c1c629a00b336
ShojiMakotoX <250039@jc-21.jp>
2026-08-23 20:04:34 +0900

    ベクトルの減算
    ベクトルの減算について学ぶ
    これで、c＃スクリプトの基礎は全てできた

--- Assets/Scenes/test.cs
+++ Assets/Scenes/test.cs
@@ -9,10 +9,14 @@ public class test : MonoBehaviour
 
     void Start()
     {
-        Vector2 playerPos = new Vector2(3.0f, 4.0f);//ベクトルを作る
-        playerPos.x += 8.0f;
-        playerPos.y += 5.0f;
-        Debug.Log(playerPos);
+        Vector2 startPos = new Vector2(2.0f, 1.0f);//ベクトルを作る
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos;//ベクトル同士の減算を行う
+        Debug.Log(dir);
+
+        float len = dir.magnitude;//magnitudeは、dirの長さに等しい。
+        Debug.Log(len);
+        
 
     }
 
