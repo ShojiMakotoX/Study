f0ad1dc76887df20817ba9476b7ef3658bd0093a
ShojiMakotoX <250039@jc-21.jp>
2026-08-23 19:06:15 +0900

    for文を確認
    For文のやり方を、C++の時と比較①
    結果　そんなに変わっていない

--- Assets/Scenes/test.cs
+++ Assets/Scenes/test.cs
@@ -5,18 +5,9 @@ public class test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int hp = 180;
-        if(hp<=50)
+        for(int i = 3;i<5;i++)
         {
-            Debug.Log("逃走");
-        }
-        else if(hp>= 200)
-        {
-            Debug.Log("攻撃");
-        }
-        else
-        {
-            Debug.Log("防御");
+            Debug.Log(i);
         }
 
 
