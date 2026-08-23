e35da2d7abeb1a3e472a6a8e3695e5847a513ade
ShojiMakotoX <250039@jc-21.jp>
2026-08-23 18:46:01 +0900

    If分のお試し
    教科書のIF分お試し

--- Assets/Scenes/test.cs
+++ Assets/Scenes/test.cs
@@ -5,10 +5,11 @@ public class test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        string str = "Happy";
-        int num = 123;
-        string message = str + num;
-        Debug.Log(message);
+        int herbNum = 1;
+        if (herbNum == 1)
+        {
+            Debug.Log("HPが50回復");
+        }
 
 
     }
