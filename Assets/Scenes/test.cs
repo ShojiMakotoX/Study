5dfd4488d1ddb9e938e00ce761e14437ea1d1648
ShojiMakotoX <250039@jc-21.jp>
2026-08-23 19:57:39 +0900

    ベクトルを作る
    ベクトルクラスをお試し

--- Assets/Scenes/test.cs
+++ Assets/Scenes/test.cs
@@ -3,29 +3,16 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    public class Player//プレイヤークラスを作る（ここはc＋＋とはめちゃ違っているなと思う）
-    {
-        private int hp = 100;
-        private int power = 50;
-
-        public void Attack()//Attack関数の処理
-        {
-            Debug.Log(this.power + "のダメージを与えた");
-        }
-
-        public void Damage(int damage)//Damage関数の処理
-        {
-            this.hp -= damage;
-            Debug.Log(damage + "のダメージを受けた");
-        }
-    }
+   
+    
 
 
     void Start()
     {
-        Player myPlayer = new Player();//プレイヤークラスが呼び出される
-        myPlayer.Attack();//アタック関数が呼び出される
-        myPlayer.Damage(30);//ダメージ関数が呼び出される
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);//ベクトルを作る
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
 
     }
 
