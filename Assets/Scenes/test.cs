4cf1a839e6fa0cbf2d265b25f6d18c49f3a9dc29
ShojiMakotoX <250039@jc-21.jp>
2026-08-23 19:19:43 +0900

    配列確認とコメント確認
    配列確認①と、コメントの仕様の復習

--- Assets/Scenes/test.cs
+++ Assets/Scenes/test.cs
@@ -6,12 +6,19 @@ public class test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int sum = 0;
-        for(int i = 1;i<=10;i++)
+        int[] array = new int[5];//配列作成
+        //データ入力
+        array[0] = 2;
+        array[1] = 10;
+        array[2] = 5;
+        array[3] = 15;
+        array[4] = 3;
+
+        //纏めて表示
+        for(int i = 0;i<5;i++)
         {
-            sum += i;
+            Debug.Log(array[i]);
         }
-        Debug.Log(sum);
 
 
     }
