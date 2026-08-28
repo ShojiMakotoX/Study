615711dd385cd3f7b9e14d03cf06f58a105110b4
ShojiMakotoX <250039@jc-21.jp>
2026-08-28 09:16:32 +0900

    コントロールのスクリプトを設定
    ルーレットを動かすためのスクリプト
    これだけではまだ動かない（次のコミットでやり方記載する予定）

---
+++ Assets/Scenes/RouletteControll.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;//入力を検知するために必要。
+
+public class RouletteControll : MonoBehaviour
+{
+    float rotspeed = 0;//回転速度の初期設定
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //フレームレートを60に固定する
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マウスが押されたら回転速度を設定する
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            this.rotspeed = 10;
+        }
+        //回転速度分、ルーレットを回転させる
+        transform.Rotate(0, 0, this.rotspeed);
+    }
+}
